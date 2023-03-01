99edf938d0b899a2d3f66d3fc450642feb9165c7
LaneBoy <kolomyjcevvladislav90@gmail.com>
2023-03-01 17:12:26 +0500

    Initial commit

---
+++ app/src/main/java/ru/laneboy/coroutinestart/MainActivity.kt
@@ -0,0 +1,56 @@
+package ru.laneboy.coroutinestart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.core.view.isVisible
+import ru.laneboy.coroutinestart.databinding.ActivityMainBinding
+import kotlin.concurrent.thread
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        binding.btnDownload.setOnClickListener {
+            loadData()
+        }
+    }
+
+    private fun loadData() {
+        binding.progressBar.isVisible = true
+        binding.btnDownload.isEnabled = false
+        loadCity {
+            binding.tvLocation.text = it
+            loadTemperature(it) {
+                binding.tvTemperature.text = it.toString()
+                binding.progressBar.isVisible = false
+                binding.btnDownload.isEnabled = true
+            }
+
+        }
+    }
+
+    private fun loadCity(callback: (String) -> Unit) {
+        thread {
+            Thread.sleep(5000)
+            callback.invoke("Moscow")
+        }
+    }
+
+    private fun loadTemperature(city: String, callback: (Int) -> Unit) {
+        thread {
+            Toast.makeText(
+                this,
+                "Loading temperature for city: $city",
+                Toast.LENGTH_SHORT
+            ).show()
+            Thread.sleep(5000)
+            callback.invoke(17)
+        }
+    }
+}
